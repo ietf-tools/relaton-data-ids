7e119abf954732f00040988b1ff64d47bf18b6c5
Stefano <ssteee@gmail.com>
2023-11-29 19:52:10 +0000

    chore: remove data directories before fetching new data

--- crawler.rb
+++ crawler.rb
@@ -3,4 +3,8 @@ require 'fileutils'
 system("rsync -avcizxL rsync.ietf.org::bibxml-ids ./bibxml-ids")
 
 require "relaton_ietf"
+
+FileUtils.rm Dir.glob("index*")
+FileUtils.rm_rf "data"
+
 RelatonIetf::DataFetcher.fetch "ietf-internet-drafts"
